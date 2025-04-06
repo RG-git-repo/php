ac1e738ae77f20aea1e2b3deed5ea8bc523796d1
RG-git-repo <83220237+RG-git-repo@users.noreply.github.com>
2025-04-06 17:36:33 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,4 +3,24 @@ echo "Hello world!!";
   
 phpinfo();
 
-?>
+$url = "https://localhost:8843/oauth2/userinfo";
+
+$ch = curl_init($url);
+
+// Ignore SSL certificate errors
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+
+// Return the response instead of printing it
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$response = curl_exec($ch);
+
+if (curl_errno($ch)) {
+    echo "cURL Error: " . curl_error($ch);
+} else {
+    echo "Response:\n";
+    echo $response;
+}
+
+curl_close($ch);
