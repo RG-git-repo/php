7c5ebadfcf05c80f96d917861b62edb11ec255d1
RG-git-repo <83220237+RG-git-repo@users.noreply.github.com>
2025-04-06 20:07:02 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,14 @@
 echo "Current Cookies:\n";
 print_r($_COOKIE);
 
+// Get all headers
+$headers = getallheaders();
+
+// Print all headers
+echo '<pre>';
+print_r($headers);
+echo '</pre>';
+
 // Try to get the _oauth_proxy cookie from the array
 $cookieValue = $_COOKIE['_oauth_proxy'] ?? 'your_cookie_value_here';
 
